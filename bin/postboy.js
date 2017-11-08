b4da708ae2cddf0dc37d832a3db9b49295e7b6fc
andrienko <evolmate@gmail.com>
2017-11-09 01:57:39 +0200

    added help and version

--- bin/postboy.js
+++ bin/postboy.js
@@ -15,6 +15,12 @@ if (!fs.existsSync(config_filename)) config_filename = path.resolve(cwd, 'postbo
 
 if (argv._[0] === 'init') {
   require('./init.js')(path.resolve(cwd, (argv._[1] || '.').toLowerCase()));
+} else if(argv._[0] === 'h' || argv['h'] || argv['help'] || argv._[0] === 'help' || argv._[0] === '?') {
+  var help = fs.readFileSync(path.resolve(__dirname,'./help.txt'),'utf-8');
+  console.log(help);
+} else if(argv._[0] === 'v' || argv['v'] || argv['version'] || argv._[0] === 'version') {
+  var pkg = require('../package.json');
+  console.log('v' + pkg.version);
 } else {
 
   if (fs.existsSync(config_filename)) {
